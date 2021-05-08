1e4b6087afc2ca121f104be006c1e0da38b01e42
Navjot Bansal <navjotbansal09@gmail.com>
2021-05-08 12:29:11 +0530

    product section define

---
+++ JalPurthi/base.js
@@ -0,0 +1,46 @@
+var body = document.body;
+	var html = document.documentElement;
+	var nav = document.querySelector(".navbar");
+
+	/* populating navbar & footer navigation*/
+	var navItems = document.querySelector("#navitems");
+	var footerItems = document.querySelector("ul#footeritems.footernavigation");
+	var unorderedList = document.createElement("ul");
+	var navItemsList = [{text:"HOME",url:"index.html"},{text:"ABOUT US",url:"about.html"},{text:"PRODUCTS",url:"index.html#producthead"},{text:"CONTACT US",url:"contact.html"}];
+	for(var i=0;i<navItemsList.length;i++)
+	{
+		unorderedList.innerHTML+= '<li> <a href="'+navItemsList[i].url+'">'+navItemsList[i].text+'</a></li>';
+		footerItems.innerHTML+= '<li> <a href="'+navItemsList[i].url+'">'+navItemsList[i].text+'</a></li>';
+	}
+	console.log(footeritems.innerHTML);
+	navitems.appendChild(unorderedList);
+	/* populating navbar & footer navigation ends */
+
+
+	/* readmore onclick */
+	var readMore = document.querySelector("#readmore");
+	if(readMore!=null)
+	{
+		readmore.addEventListener("click",function()
+		{
+			window.location.replace("about.html");
+		})	
+	}
+	/* readmore onclick ends*/
+
+	var bodycontainer = document.querySelector(".bodycontainer");
+	if(bodycontainer!=null)
+	{
+		bodycontainer.style.marginTop = nav.offsetHeight+"px";	
+	}
+	window.onscroll = function()
+		{
+			if(html.scrollTop>0 || body.scrollTop>0)
+			{
+				nav.style.boxShadow="0px 1px 5px #999";
+			}
+			else
+			{
+					nav.style.boxShadow="none";
+			}
+		}
