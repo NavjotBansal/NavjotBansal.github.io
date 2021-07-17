16a3268038689d42c9833ca534425468a75b9f37
Navjot Bansal <navjotbansal09@gmail.com>
2021-07-17 10:46:35 +0530

    fixing bugs

--- JalPurthi/base.js
+++ JalPurthi/base.js
@@ -70,7 +70,7 @@ var body = document.body;
 				navigation.style.marginTop=-1*navoffset+"px";
 		}
 		else
-			navigation.style.marginTop="-100%";
+			navigation.style.marginTop="-200%";
 		isclosed=!isclosed;
 	});
 	background.addEventListener("click",function(){
@@ -82,6 +82,6 @@ var body = document.body;
 				navigation.style.marginTop=-1*navoffset+"px";
 		}
 		else
-			navigation.style.marginTop="-100%";
+			navigation.style.marginTop="-200%";
 		isclosed=!isclosed;
 	});
